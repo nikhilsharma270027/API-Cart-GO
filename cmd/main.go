61b9b8098d0ed25c41e99aa9a54b99507b9af67a
nikhil <nikhilsharma270027@gmail.com>
2025-01-18 23:22:52 +0530

    add config

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,28 @@ package main
 import (
 	"log"
 
+	"github.com/go-sql-driver/mysql"
 	"github.com/nikhilsharma270027/API-Cart-GO/cmd/api"
+	"github.com/nikhilsharma270027/API-Cart-GO/config"
+	"github.com/nikhilsharma270027/API-Cart-GO/db"
 )
 
 func main() {
-	server := api.NewAPIServer(":8080", nil)
+	db, err := db.NewMySQLStorage(mysql.Config{
+		User:                 config.Envs.DBUser,
+		Passwd:               config.Envs.DBPassword,
+		Addr:                 config.Envs.DBAddress,
+		DBName:               config.Envs.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	// server := api.NewAPIServer(":8080", nil) adding db
+	server := api.NewAPIServer(":8080", db)
 	if err := server.Run(); err != nil {
 		log.Fatal("Server Running on port ")
 	}
